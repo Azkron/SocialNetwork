f381570fd9eee032a8242ed3be9dd1531613594c
2610titoure <2610titoure@PC8901.domepfc.net>
2016-12-15 10:41:50 +0100

    Commmit modif dev.ini + prwb_calendar

--- database/prwb_1617_G10.sql
+++ database/prwb_1617_G10.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `prwb_calendar_GXX`
 --
-CREATE DATABASE IF NOT EXISTS `prwb_calendar_GXX` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `prwb_calendar_G10` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `prwb_calendar_G10`;
 
 -- --------------------------------------------------------
