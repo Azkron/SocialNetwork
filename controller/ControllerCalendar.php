50dcb9a0d5ee81af9bfc9401f9793afdd3b05aa1
AZKRON\Hugo <hugobeny@gmail.com>
2017-01-26 10:53:59 +0100

    more done on Calendar.php and controller CAlenad

--- controller/ControllerCalendar.php
+++ controller/ControllerCalendar.php
@@ -1,8 +1,78 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'model/Member.php';
+require_once 'model/Message.php';
+require_once 'framework/View.php';
+require_once 'framework/Controller.php';
 
+class ControllerMember extends Controller {
+    
+    const UPLOAD_ERR_OK = 0;
+
+    //gestion de l'édition du profil
+    public function edit_profile() {
+        $member = $this->get_user_or_redirect();
+        $error = "";
+        $success = "";
+
+        // Il est néces
+
+        if (isset($_POST['profile'])) {
+            $profile = $_POST['profile'];
+            $member->profile = $profile;
+            $member->update();
+            $success = "Your profile has been successfully updated.";
+        }
+        (new View("edit_profile"))->show(array("member" => $member, "error" => $error, "success" => $success));
+    }
+
+    //page d'accueil. 
+    public function index() {
+        $this->my_calendars();
+    }
+
+    //profil de l'utilisateur connecté ou donné
+    public function my_calendars() {
+        $user = $this->get_user_or_redirect();
+        (new View("MyCalendars"))->show(Calendar::get_calendars($user));
+    }
+
+    //gestion du suivi d'un membre
+    public function delete() {
+        $member = $this->get_user_or_redirect();
+        if (isset($_POST["follow"]) && $_POST["follow"] != "") {
+            $followee_pseudo = $_POST["follow"];
+            $followee = Member::get_member($followee_pseudo);
+            $member->follow($followee);
+            $this->redirect("member", "members");
+        } else {
+            throw new Exception("Missing ID");
+        }
+    }
+    
+    //gestion du suivi d'un membre
+    public function confirm_delete() {
+        $member = $this->get_user_or_redirect();
+        if (isset($_POST["follow"]) && $_POST["follow"] != "") {
+            $followee_pseudo = $_POST["follow"];
+            $followee = Member::get_member($followee_pseudo);
+            $member->follow($followee);
+            $this->redirect("member", "members");
+        } else {
+            throw new Exception("Missing ID");
+        }
+    }
+    
+    public function edit() {
+        $member = $this->get_user_or_redirect();
+        if (isset($_POST["follow"]) && $_POST["follow"] != "") {
+            $followee_pseudo = $_POST["follow"];
+            $followee = Member::get_member($followee_pseudo);
+            $member->follow($followee);
+            $this->redirect("member", "members");
+        } else {
+            throw new Exception("Missing ID");
+        }
+    }
+
+}
--- view/view_MyCalendars.php
+++ view/view_MyCalendars.php
@@ -1,8 +1,69 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Calendars</title>
+        <base href="<?= $web_root ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="title">My Calendars</div>
+        <div class="menu">
+            <a href="index.php">Home</a>
+        </div>
+        <div class="main">
+            <br><br>
+            <form id="signupForm" action="main/signup" method="post">
+                <table>
+                    <tr>
+                        <td>Description</td>
+                        <td>Color</td>
+                        <td>Actions</td>
+                    </tr>
+                    <?php if (count($errors) != 0): ?>
+                        <div class='errors'>
+                            <br><br><p>Please correct the following error(s) :</p>
+                            <ul>
+                                <?php foreach ($errors as $error): ?>
+                                    <li><?= $error ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+                    <tr>
+                        <td>Full Name:</td>
+                        <td><input id="full_name" name="full_name" type="text" size="16" value="<?= $full_name ?>"></td>
+                        <td class="errors" id="errFullName"></td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><input id="email" name="email" type="text" size="16" value="<?= $email ?>"></td>
+                        <td class="errors" id="errEmail"></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input id="password" name="password" type="password" size="16" value="<?= $password ?>"></td>
+                        <td class="errors" id="errPassword"></td>
+                    </tr>
+                    <tr>
+                        <td>Confirm Password:</td>
+                        <td><input id="passwordConfirm" name="password_confirm" size="16" type="password" value="<?= $password_confirm ?>"></td>
+                        <td class="errors" id="errPasswordConfirm"></td>
+                    </tr>
+                </table>
+                <input id="btn" type="submit" value="Sign Up">
+            </form>
+            <?php if (count($errors) != 0): ?>
+                <div class='errors'>
+                    <br><br><p>Please correct the following error(s) :</p>
+                    <ul>
+                        <?php foreach ($errors as $error): ?>
+                            <li><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+        </div>
+    </body>
+</html>
